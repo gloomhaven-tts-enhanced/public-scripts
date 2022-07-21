83357e376767da86e4fa3db87793a2584b53e404
Sebastian Stern <sebastian.stern.42@gmail.com>
2022-07-21 10:27:32 +0200

    docs: More scenario stuff

---
+++ docs/content/customContent/modules/ROOT/examples/scenario/scenario-book/usage.lua
@@ -0,0 +1,8 @@
+scenarioBook = {
+  -- The scenario book to use
+  name = "Gloomhaven",
+  -- The page that will be opened inside the book
+  page = 3,
+  -- If 2 pages should be shown, set this value as well
+  pageCount = 2,
+}
